d8dea14be9fafde632de10c6c2157738954969bf
muhaha120054 <43543025+muhaha120054@users.noreply.github.com>
2018-11-22 08:18:50 +0800

    Add files via upload

---
+++ meal.js
@@ -0,0 +1,54 @@
+const menu = {
+  _courses: {
+  appetizers:[],
+  mains:[],
+  desserts:[],
+  },
+
+  get appetizers(){
+    return appetizers
+  },
+  
+  set appetizers(appetizerIn){
+    return appetizerIn;
+  },
+  get courses(){
+    return {
+      appetizers: this._courses.appetiers,
+      mains: this._courses.mains,
+      desserts: this._courses.desserts,
+    }
+  },
+  
+  addDishToCourse(courseName, dishName, dishPrice){
+    const dish = {
+      name:{dishName},
+      price:{dishPrice},
+    }
+    this._courses[courseName].push(dish);
+  },
+  getRandomDishFromCourses(courseName){
+    const dishes = this._courses[courseName];
+    const randomIndex = Math.floor(Math.random() * dishes.length);
+  },
+	generateRandomMeal(){
+    const appetizer = this.getRandomDishFromCourses('Appetizers');
+    const mains = this.getRandomDishFromCourses('mains');
+    const desserts = this.getRandomDishFromCourses('desserts');
+    const totalPrice = appetizer.price + mains.price + desserts.price;
+    return `Your meal is ${appetizer.name}, ${main.name}, ${desserts.name}. The price is $${totalPrice}.`;
+  }
+};
+
+menu.addDishToCourse('appetizers','Casar Salad', 4.25);
+menu.addDishToCourse('appetizers','Fried Fries', 3.25);
+menu.addDishToCourse('appetizers','Fried Chiken Stick', 5.25);
+menu.addDishToCourse('mains','steak 7 once', 9.25);
+menu.addDishToCourse('mains','steak 12 once', 12.25);
+menu.addDishToCourse('mains','steak  16 once', 20.25);
+menu.addDishToCourse('desserts','Ice cream', 1.25);
+menu.addDishToCourse('desserts','Cheese Cake', 1.25);
+menu.addDishToCourse('desserts','Banana Cake', 1.25);
+
+let meal = menu.generateRandomMeal();
+console.log(meal);
