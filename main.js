3cb18d99805f722beb451bc1e1efa56fa4bcba2a
muhaha120054 <43543025+muhaha120054@users.noreply.github.com>
2018-12-04 09:51:44 +0800

    formal adjustments
    abv.

--- main.js
+++ main.js
@@ -1,56 +1,60 @@
 function getRecordFromTxtFile(){
+    
+    //read from txt file
     const fs = require('fs');
     let txtFile = "AGL_001.TXT";
     let str = fs.readFileSync(txtFile,'utf8');
-    let newString =[];
+    
 
-    // 9 = tab, 32 = space ,13 = enter ,10 = nothing
+    //turn the followings into hypen
+    // charcode: 9 = tab, 32 = space ,13 = enter ,10 = nothing
+    let tempString =[];
     for(let i =0;i<str.length;i++){
         let x = str.charCodeAt(i);
         if( x === 9 || x === 32 || x === 13 || x ==="\n"){
-            newString += "-";
+            tempString += "-";
         } else{
-          newString += str.charAt(i);
+            tempString += str.charAt(i);
         }
     }
-    let records = newString.split('-');
-    let record = new Array();
-    let row = 0;
-
+  
+    //extract useful elements to cleanSet
+    let tempArray = tempString.split('-');
+    let cleanSet = new Array();
     let element =0;
-    for(let i =7;i<records.length;i++){
+    console.log(tempArray[7].substring(1));
+    for(let i =7;i<tempArray.length;i++){
         if(i%8 === 7){
-            records[i] = records[i].substring(1);    
-            //console.log(typeof records[i]);
-            //console.log(record[row][element]);
-            record[element] = records[i];       
+            tempArray[i] = tempArray[i].substring(1);
+            cleanSet[element] = tempArray[i];       
             element += 1;
         } else if(i%8 === 1){
-            record[element] = records[i];       
+            cleanSet[element] = tempArray[i];       
             element += 1;
         }else if(i%8 === 5){
-            record[element] = records[i];       
+            cleanSet[element] = tempArray[i];       
             element += 1;
         }else if(i%8 === 6){
-            record[element] = records[i];       
+            cleanSet[element] = tempArray[i];       
             element += 1; 
         }
     }
-    let data = [];
+
+    //recombine elements into record[indexID, studentID,Date,Time]
+    let record = [];
     let index =0;
-    
-    for ( let i =0;i<(record.length/4);i++){
+    for ( let i =0;i<(cleanSet.length/4);i++){
         for( let j =0;j< 4;j++)
         {
             if(j === 0){
-                data[i] = new Array(4);    
+                record[i] = new Array(4);    
             }
-            data[i][j] = record[index];
+            record[i][j] = cleanSet[index];
             index += 1;
         }
     }
     //test result
-    console.log(data[1313]);
+    console.log(record[1]);
 }
 function setUpTeacherData(){
     let Teacher = function (id, name,salaryType){
